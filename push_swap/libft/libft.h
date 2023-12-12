2babb424268acfeb777a63e73a4e48b78e85f8bc
Pablo Rando Alcala <prando-a@c2r2s3.42malaga.com>
2023-12-12 18:47:47 +0100

    push_swap hotfix

--- push_swap/libft/libft.h
+++ push_swap/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: prando-a <prando-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 17:19:02 by prando-a          #+#    #+#             */
-/*   Updated: 2023/12/12 14:50:48 by prando-a         ###   ########.fr       */
+/*   Updated: 2023/12/12 18:47:11 by prando-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 # include <sys/stat.h>
 # include <fcntl.h>
 # include "ft_printf/ft_printf.h"
-# include "push_swap.h"
+# include "../push_swap.h"
 
 // typedef struct s_list
 // {
